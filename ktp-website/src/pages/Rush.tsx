2aa4f6a7457b6b0c25ef2634b40f727543ed2045
AndersonBlanco <andersonblanco913@gmail.com>
2026-09-05 17:43:28 -0400

    Filter rush events to only render those with visibility=public

--- ktp-website/src/pages/Rush.tsx
+++ ktp-website/src/pages/Rush.tsx
@@ -286,8 +286,8 @@ function Rush() {
                     <h2 className="text-center text-2xl sm:text-4xl font-black text-ktp-appblue mb-10">
                         Upcoming Rush Events
                     </h2>
-                    {/* Pass your "events" state directly to RushEvents */}
-                    <RushEvents events={events} />
+                    {/* Pass only public events to RushEvents */}
+                    <RushEvents events={events.filter((e: any) => e.visibility === "public")} />
                 </div>
                 )}
 
